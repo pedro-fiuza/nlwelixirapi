b07f0403b7652b6c84c11e1faa9810ecc7150fb1
pedro-fiuza <pdrfiuza@gmail.com>
2021-02-27 16:40:05 -0300

    pipeoperator e pattern matching com a regra de negocio

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,18 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, file}) do
+    result =
+      file
+      |> String.split(",")
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+  defp handle_file({:error, _reason}), do: {:error, "Arquivo invalido"}
+end
